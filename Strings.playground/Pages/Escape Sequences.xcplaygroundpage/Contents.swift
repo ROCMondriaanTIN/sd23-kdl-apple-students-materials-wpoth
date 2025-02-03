8f601c9d730b95d19478c9a9a99efe41e49c90ba
wpoth <143715561+wpoth@users.noreply.github.com>
2025-02-03 13:04:38 +0100

    Add files via upload

---
+++ Strings.playground/Pages/Escape Sequences.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+/*:
+## Escape Sequences
+When you type an _escape character_ followed by something that’s treated specially, you're creating an _escape sequence_. You’ve already seen two different kinds of escape sequences:
+ */
+// The backslash followed by a quotation mark is an escape sequence.
+let favoriteQuotation = "Hamlet said, \"To be, or not to be?\""
+
+// Another escape sequence is the placeholder in an interpolated string.
+let flavor = "chocolate"
+let iceCreamAnnouncement = "The flavor of the day is \(flavor)"
+
+
+let startOfAPoem = ""
+/*:
+In the result displayed above, notice a new line starts where the `\n` appears in the string.
+
+ - Note: If the result isn't visible, click the show result button for the line of code above.
+
+Also notice that the string in the results sidebar shows the `\n` instead of skipping a line.\
+That’s because the results sidebar can only use one line to show results.
+
+
+Move on to the next page to wrap things up.
+
+[Previous](@previous)  |  page 12 of 18  |  [Next: Wrapping Up](@next)
+ */
